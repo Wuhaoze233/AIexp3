f6efe304c7eebc561927c8bf9a6eab78fefad2ca
Littlesock <144090458+Wuhaoze233@users.noreply.github.com>
2025-05-13 18:31:29 +0800

    ID3树，加入了数据预处理，测试后发现行不通，得重新改

--- ID3.py
+++ ID3.py
@@ -120,24 +120,34 @@ def createTree(dataSet, labels, featLabels):
 
 
 if __name__ == '__main__':
-    dataSet = [[0, 0, 0, 0, 'no'],  # 数据集
-               [0, 0, 0, 1, 'no'],
-               [0, 1, 0, 1, 'yes'],
-               [0, 1, 1, 0, 'yes'],
-               [0, 0, 0, 0, 'no'],
-               [1, 0, 0, 0, 'no'],
-               [1, 0, 0, 1, 'no'],
-               [1, 1, 1, 1, 'yes'],
-               [1, 0, 1, 2, 'yes'],
-               [1, 0, 1, 2, 'yes'],
-               [2, 0, 1, 2, 'yes'],
-               [2, 0, 1, 1, 'yes'],
-               [2, 1, 0, 1, 'yes'],
-               [2, 1, 0, 2, 'yes'],
-               [2, 0, 0, 0, 'no']]
-
-    labels = ['年龄', '有工作', '有自己的房子', '信贷情况']  # 特征标签
-    labels1 = ['放贷', '不放贷']
+    # dataSet = [[0, 0, 0, 0, 'no'],  # 数据集
+    #            [0, 0, 0, 1, 'no'],
+    #            [0, 1, 0, 1, 'yes'],
+    #            [0, 1, 1, 0, 'yes'],
+    #            [0, 0, 0, 0, 'no'],
+    #            [1, 0, 0, 0, 'no'],
+    #            [1, 0, 0, 1, 'no'],
+    #            [1, 1, 1, 1, 'yes'],
+    #            [1, 0, 1, 2, 'yes'],
+    #            [1, 0, 1, 2, 'yes'],
+    #            [2, 0, 1, 2, 'yes'],
+    #            [2, 0, 1, 1, 'yes'],
+    #            [2, 1, 0, 1, 'yes'],
+    #            [2, 1, 0, 2, 'yes'],
+    #            [2, 0, 0, 0, 'no']]
+    #
+    # labels = ['年龄', '有工作', '有自己的房子', '信贷情况']  # 特征标签
+    # labels1 = ['放贷', '不放贷']
+    # featLabels = []
+    # myTree = createTree(dataSet, labels, featLabels)
+    # print(myTree)
+    dataset = []
+    division = []
+    dataset, division = preprocessDataSet('traindata.txt')
+    print(dataset)
+    print(division)
+    labels = ['a1', 'b2', 'c3', 'd4']
+    labels1 = ['0', '1', '2']
     featLabels = []
-    myTree = createTree(dataSet, labels, featLabels)
-    print(myTree)
+    # myTree = createTree(dataset, labels, featLabels)
+    # print(myTree)
